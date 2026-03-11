ae1a1da87b66c8974aaf375f67a67a7df6ed32ac
Eric Wilson <wilser@gmail.com>
2026-03-10 18:48:55 -0700

    (CC) NUM change hold-texts around to try if more useful

--- config/user/macros/macro_ht_data.h
+++ config/user/macros/macro_ht_data.h
@@ -1,16 +1,15 @@
 // === HOLD-TAP MACRO DATA ===
 // X-macro table: HT_MACRO_ENTRY(NAME, <&kp ...>, ...)
-// Included twice:
-//   - in macros {}   (via MACRO_KPS alias)  → generates m_NAME zmk macro nodes
-//   - in behaviors{} (via HT_FROM_MACRO alias) → generates ht_NAME hold-tap nodes
+// Included once in macros {} — generates both m_NAME (macro) and ht_NAME (hold-tap) nodes.
 // To add/change an entry: edit only this file.
 
 //                   NAME          key sequence (hold output)
-HT_MACRO_ENTRY(1st,        <&kp S>, <&kp T>)                          // st
-HT_MACRO_ENTRY(2nd,        <&kp N>, <&kp D>)                          // nd
-HT_MACRO_ENTRY(3rd,        <&kp R>, <&kp D>)                          // rd
-HT_MACRO_ENTRY(4th,        <&kp T>, <&kp H>)                          // th
-HT_MACRO_ENTRY(am_lc,      <&kp A>, <&kp DOT>, <&kp M>, <&kp DOT>)    // a.m.
-HT_MACRO_ENTRY(pm_lc,      <&kp P>, <&kp DOT>, <&kp M>, <&kp DOT>)    // p.m.
-HT_MACRO_ENTRY(hr_abbrev,  <&kp H>, <&kp R>)                          // hr
-HT_MACRO_ENTRY(min_abbrev, <&kp M>, <&kp I>, <&kp N>)                 // min
+HT_MACRO_ENTRY(1st,        <&kp F>, <&kp I>, <&kp R>, <&kp S>, <&kp T>)                          // first
+HT_MACRO_ENTRY(2nd,        <&kp S>, <&kp E>, <&kp C>, <&kp O>, <&kp N>, <&kp D>)                 // second
+HT_MACRO_ENTRY(3rd,        <&kp T>, <&kp H>, <&kp I>, <&kp R>, <&kp D>)                          // third
+HT_MACRO_ENTRY(4th,        <&kp F>, <&kp O>, <&kp U>, <&kp R>, <&kp T>, <&kp H>)                 // fourth
+HT_MACRO_ENTRY(am_lc,      <&kp SPACE>, <&kp A>, <&kp DOT>, <&kp M>, <&kp DOT>)                  //  a.m.
+HT_MACRO_ENTRY(pm_lc,      <&kp SPACE>, <&kp P>, <&kp DOT>, <&kp M>, <&kp DOT>)                  //  p.m.
+HT_MACRO_ENTRY(am_uc,      <&kp SPACE>, <&kp LS(A)>, <&kp LS(M)>)                                //  AM
+HT_MACRO_ENTRY(pm_uc,      <&kp SPACE>, <&kp LS(P)>, <&kp LS(M)>)                                //  PM
+HT_MACRO_ENTRY(minute,     <&kp M>, <&kp I>, <&kp N>, <&kp U>, <&kp T>, <&kp E>)                 // minute
